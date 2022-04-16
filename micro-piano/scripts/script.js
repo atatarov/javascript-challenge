695d796b728ca3d7ceda6e651b3f328f06000011
Aleksey Tatarov <alekseytatarv@gmail.com>
2022-04-17 02:28:25 +0400

    Add micro-piano project
    Add pet-project that shows how you can rule mouse clicks and keyboard events.

---
+++ micro-piano/scripts/script.js
@@ -0,0 +1,33 @@
+function playAudioByKey(key) {
+  const audio = document.querySelector(`audio[data-key="${key}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function addTransition(key) {
+  const keyElement = document.querySelector(`.key[data-key="${key}"]`);
+  keyElement.classList.add("playing");
+  const spanElement = keyElement.querySelector("span");
+  spanElement.classList.add("playing-span");
+}
+
+window.addEventListener("keydown", function (event) {
+  playAudioByKey(event.code);
+});
+
+window.addEventListener("click", function (event) {
+  const dataKey = event.target.getAttribute("data-key");
+  playAudioByKey(dataKey);
+});
+
+function removeTransition(event) {
+  if (event.propertyName != "background-color") return;
+
+  this.classList.remove("playing");
+  const spanElement = this.querySelector("span");
+  spanElement.classList.remove("playing-span");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
